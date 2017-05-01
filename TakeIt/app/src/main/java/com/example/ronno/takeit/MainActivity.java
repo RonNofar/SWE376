f63289de5f2b255f9ec58158eda19fcfb83958c3
RonNofar <ronnofar2@gmail.com>
2017-05-01 03:18:20 -0700

    Record and Stop Buttons
    Added a record button and a stop recording button, still does not save anywhere.

--- TakeIt/app/src/main/java/com/example/ronno/takeit/MainActivity.java
+++ TakeIt/app/src/main/java/com/example/ronno/takeit/MainActivity.java
@@ -4,8 +4,10 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,5 +39,53 @@ public class MainActivity extends AppCompatActivity {
         stopButton.setEnabled(false);
         playButton.setEnabled(false);
         matchButton.setEnabled(false);
+
+        initializeMediaRecord();
+
+        recordingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaRecorder == null) {
+                    initializeMediaRecord();
+                }
+                startAudioRecording();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAudioRecording();
+            }
+        });
+    }
+
+    private void startAudioRecording() {
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recordingButton.setEnabled(false);
+        stopButton.setEnabled(true);
+    }
+
+    private void stopAudioRecording() {
+        if(mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
+        stopButton.setEnabled(false);
+        playButton.setEnabled(true);
+    }
+
+    private void initializeMediaRecord() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(voiceStoragePath);
     }
 }
